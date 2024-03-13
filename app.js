06440c56d10b97e265164ea1187388355ddcfd0a
Akbar Rahmat Mulyatama <88664674+akbarrahmatm@users.noreply.github.com>
2024-03-13 22:21:20 +0700

    Feature : get data dummy from json

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
+const fs = require("fs");
 const express = require("express");
 
 const app = express();
 const PORT = 3000;
 
+const customers = JSON.parse(fs.readFileSync(`${__dirname}/data/dummy.json`));
+
 app.get("/", (req, res, next) => {
   res.send("<h1>Hola</h1>");
 });
 
-app.get("/data", (req, res, next) => {
-  res.send("<h1>Hola</h1>");
+app.get("/api/v1/customers", (req, res, next) => {
+  res.status(200).json({
+    status: "Success",
+    data: { customers },
+  });
 });
 
 app.listen(PORT, () => {
