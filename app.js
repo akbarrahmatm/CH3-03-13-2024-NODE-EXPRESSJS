c80b9f71b47bd7dc7f818adec7d54f1442de3f3a
Akbar Rahmat Mulyatama <88664674+akbarrahmatm@users.noreply.github.com>
2024-03-14 22:52:33 +0700

    Feature : delete data by id, refactor code

--- app.js
+++ app.js
@@ -9,31 +9,28 @@ app.use(express.json());
 
 const customers = JSON.parse(fs.readFileSync(`${__dirname}/data/dummy.json`));
 
-app.get("/", (req, res, next) => {
+const defaultRouter = (req, res, next) => {
   res.send("<h1>Hola</h1>");
-});
+};
 
-// Get data
-app.get("/api/v1/customers", (req, res, next) => {
+const getAllCustomer = (req, res, next) => {
   res.status(200).json({
     status: "Success",
     totalData: customers.length,
     data: { customers },
   });
-});
+};
 
-// Get data by id
-app.get("/api/v1/customers/:id", (req, res, next) => {
+const getCustomerById = (req, res, next) => {
   const customer = customers.find((custData) => custData._id === req.params.id);
 
   res.status(200).json({
     status: "Success",
     data: { customer },
   });
-});
+};
 
-// Update data
-app.patch("/api/v1/customers/:id", (req, res, next) => {
+const updateCustomer = (req, res, next) => {
   const id = req.params.id;
 
   // Search ID
@@ -59,16 +56,43 @@ app.patch("/api/v1/customers/:id", (req, res, next) => {
       return res.status(200).json({
         status: "Success",
         message: "Data successfully updated",
-        // data: {
-        //   customer: customers[customerIndex],
-        // },
       });
     }
   );
-});
+};
 
-// Create data
-app.post("/api/v1/customers", (req, res, next) => {
+const deleteCustomer = (req, res, next) => {
+  const id = req.params.id;
+
+  // Search ID
+  const customer = customers.find((custData) => custData._id === id);
+  const customerIndex = customers.findIndex((custData) => custData._id === id);
+
+  // If data didn't exist
+  if (!customer) {
+    return res.status(404).json({
+      status: "Failed",
+      message: `Customer with ID '${id}' Not Found`,
+    });
+  }
+
+  // Kalau ada, delete data
+  customers.splice(customerIndex, 1);
+
+  // Update data
+  fs.writeFile(
+    `${__dirname}/data/dummy.json`,
+    JSON.stringify(customers),
+    (err) => {
+      return res.status(200).json({
+        status: "Success",
+        message: "Data successfully deleted",
+      });
+    }
+  );
+};
+
+const insertCustomer = (req, res, next) => {
   console.log(req.body);
 
   const newCust = req.body;
@@ -86,7 +110,15 @@ app.post("/api/v1/customers", (req, res, next) => {
       });
     }
   );
-});
+};
+
+// Router
+app.get("/", defaultRouter);
+app.get("/api/v1/customers", getAllCustomer);
+app.get("/api/v1/customers/:id", getCustomerById);
+app.patch("/api/v1/customers/:id", updateCustomer);
+app.patch("/api/v1/customers/delete/:id", deleteCustomer);
+app.post("/api/v1/customers", insertCustomer);
 
 app.listen(PORT, () => {
   console.log(`App running on: locahost:${PORT}`);
