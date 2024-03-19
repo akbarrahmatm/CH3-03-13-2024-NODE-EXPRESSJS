ff517a5ebb82890f6e6f2429c7b662b16601c233
Akbar Rahmat Mulyatama <88664674+akbarrahmatm@users.noreply.github.com>
2024-03-19 22:53:59 +0700

    Feature: Implement mongodb with mongooose

--- server.js
+++ server.js
@@ -1,7 +1,58 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+
 const app = require("./app");
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 
+const DB = process.env.DATABASE;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+  })
+  .then((con) => {
+    console.log("Koneksi berhasil");
+  });
+
+const customerSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name cannot be empty"],
+  },
+  email: {
+    type: String,
+    unique: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  city: String,
+  country: {
+    type: String,
+    required: true,
+    default: "Indonesia",
+  },
+});
+
+const Customer = mongoose.model("Customer", customerSchema);
+
+const customerTest = new Customer({
+  name: "Akbar Ganteng",
+  email: "akbarganteng@gmail.com",
+  phoneNumber: "082637627632",
+});
+
+customerTest
+  .save()
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(`ERROR: ${err}`);
+  });
 app.listen(PORT, () => {
   console.log(`App running on: locahost:${PORT}`);
 });
